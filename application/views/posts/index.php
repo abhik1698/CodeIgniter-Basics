b89c9fa380f23b66d2d81e016525ffea7302f13e
Abhishek K <abhik1698@gmail.com>
2019-07-23 17:47:12 +0530

    Changed index view of posts to have a link to headers!

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,9 +1,21 @@
 <h4><?= $title;?></h4>
 
 <?php foreach ($posts as $post) : ?>
-	<h2><b><?= $post['title']; ?></b></h2>
+	<?= '<div href="./posts/'.$post['slug'].'">'?>
+	<h2><b><?= '<a href="./posts/'.$post['slug'].'">'.$post['title'].'</a>' ?></b></h2>
 	<small class="post-date">Posted on: <?= $post['created_at']; ?></small><br>	
-	<h3><?= $post['body']; ?></h3>
-	<a class="btn btn-default" href="<?= site_url('./posts/'.$post['slug']);?>">Read more</a>
+	<h3><?php
+		$string = strip_tags($post['body']);
+		if (strlen($string) > 250) {
+			// truncate string
+			$stringCut = substr($string, 0, 250);
+			$endPoint = strrpos($stringCut, ' ');
+
+			//if the string doesn't contain any space then it will cut without word basis.
+			$string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+			$string .= '... <a href="./posts/'.$post['slug'].'">Read More</a>';
+		}
+		echo $string;
+	?></h3>	
 	<hr>
 <?php endforeach; ?> 
